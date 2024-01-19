a7456ca8afef37d52cbc713c90562d44a3036eca
SarahWidiPost <156486177+SarahWidiPost@users.noreply.github.com>
2024-01-19 20:22:05 +0000

    Latest build deployed.

--- lunr-pretext-search-index.js
+++ lunr-pretext-search-index.js
@@ -10,27 +10,18 @@ var ptx_lunr_docs = [
   "body": "  My Website   copyright  "
 },
 {
-  "id": "sec-units",
+  "id": "sec-units-intro",
   "level": "1",
-  "url": "sec-units.html",
-  "type": "Section",
-  "number": "1.1",
-  "title": "Units and Unit Analysis",
-  "body": " Units and Unit Analysis    Scientific progress is measured in units of courage, not intelligence. - Paul Dirac  Almost all numbers we encounter in day-to-day life (news reports, professional data, personal finances...) have units attached. The units of a quantity describe what the quantity measures or counts. Units tell us how to interpret numbers and give us awareness of scale.    Introduction to Units   discusses the formation of the Hawaiian Islands and the relation between their age and position with respect to their volcanic hot spot. We include all units in the description.   There are eight major volcanic Islands in Hawaiʻi: Niʻihau, Kauaʻi, Oʻahu, Molokaʻi , Lānaʻi, Maui, Kahoʻolawe and Hawaiʻi Island https:\/\/www.hvcb.org\/school-report\/eight-major-islands\/ . See   In addition, there are several atolls, and numerous smaller islets. It forms what we call an archipelago. Hawaiʻi became U.S. state on August 21, 1959 https:\/\/www.archives.gov\/legislative\/features\/hawaii , and is the only one not geographically connected to North America.    Map of the Hawaiian or Sandwich Islands, published in 1854 in Edward T. Perkins' Na Motu; or, Reef-Rovings in the South Seas.  Map of the Hawaiian ialnds in grey and white.   The formation of the Hawaiian Islands is occurring via Hot-Spot Volcanism. The fixed hot spot at the source of the Hawaiian Islands is located in the middle of the Pacific Plate, and the chain was formed as the plate drifted toward the northwest. Loihi, the youngest seamount of the chain is currently erupting from its summit at a depth of 1000 meters. In the following chart we have the age of some Hawaiian volcanoes along with their distance from Kilauea: https:\/\/pubs.usgs.gov\/pp\/1987\/1350\/pdf\/chapters\/pp1350_ch1.pdf      Volcano  Age in million years  Distance in km    Kilauea  0. 2  0    Mauna Kea  0.375  54    Kohala  0.43  100    Haleakala  0.75  182    Kahoolawe  1.03  185    West Maui  1.32  221    Lānaʻi  1.28  226    East Molokaʻi  1.76  256    West Molokaʻi  1.9  280    Koolau  2.6  339    Waianae  3.7  374    Kauaʻi  5.1  519    Niʻihau  5.89  565     This relation is an example of what we call a function which means that for each Island, given as input their age in million years we have an output in the form of their distance in km to the Hot Spot. It can be observed that from that the Island of Niihau is the oldest one from this list being 4.89 million years old and also the furthest one from the Hot Spot at 565 km. Koolau, Oʻahu Island, is estimated to be 2.6 million years old and at 339 km from the hot spot.    discusses a dramatic event that occurred on the Island of Kaho'olawe in Spring 2020.   In late February of 2020, Kahoʻolawe caught fire (see for an aerial view of the region that burned). Due to the danger of unexploded ordnances, firefighters were unable to actively fight the brush fire, which left it burning for roughly six days until a rainstorm stopped the spread. The fire burned much of the re-introduced vegetation on the island along with destroying the Kahoʻolawe Island Reserve Commission (KIRC) equipment and storage http:\/\/www.kahoolawe.hawaii.gov\/newsletters\/Spring_2020.pdf . is a transcript of a news report from Hawaiʻi Public Radio on the subject.      From a news report on Kahoʻolawe fire, Spring 2020 (source: Hawaiʻi Public Radio).   In that report units were critical to understand the time-line of the event, its magnitude, as well as the cost. Extracted from the text, we can see that the units quantifying the event are:  week ;  acres ;  years ;  dollars .    Aerial View of the region that caught fire. Taken by Prof. M. Chyba on January 29, 2023.     Unit Analysis   is the process of working with units to help solve problems.      Coming back to and . Answer the following questions about the units   What is the independent variable in this function? What are the units for the independent variable? Age is the independent variable and Million Years is the corresponding unit.  What is the dependent variable in this function? What are the units for the dependent variable? Distance is the dependent variable (the distance depend on the age) and kilometers (km) is the corresponding unit.  How much older and further away from the hot spot is Kauaʻi compare to Mauna Kea? Kauaʻi is  million years older than Mauna Kea  million years) and is  km further away from the volcanic hot spot (  km. ).   We finish this section with another example of units used as a descriptor of some specifics about the island of Kahoʻolawe.   Quantitative Information about Kahoʻolawe     Environmental data. The yearly temperature typically varies between F ( C) and F C) with the coolest in February and warmest in September. The largest rain precipitation is usually in January with an average of  inches (  mm ) and the driest month is June with an average of  inches (  mm ) https:\/\/geoportal.hawaii.gov\/    Geographical data. Its position on Earth is given by latitude:  N, longitude:  W. The island's dimension is  miles ( km ) long by  miles (  km ) across. It has a total land area of  square miles (  km ), and the highest elevation is the crater of Lua Makika at  feet (ft) (  meters ) >https:\/\/en.wikipedia.org\/wiki\/Kahoolawe      Unit Conversion  Text of section.   "
-},
-{
-  "id": "sec-units-2-2",
-  "level": "2",
-  "url": "sec-units.html#sec-units-2-2",
-  "type": "Paragraph (with a defined term)",
-  "number": "",
-  "title": "",
-  "body": "units units "
+  "url": "sec-units-intro.html",
+  "type": "Subsection",
+  "number": "1.1.1",
+  "title": "Introduction to Units",
+  "body": " Introduction to Units   discusses the formation of the Hawaiian Islands and the relation between their age and position with respect to their volcanic hot spot. We include all units in the description.   There are eight major volcanic Islands in Hawaiʻi: Niʻihau, Kauaʻi, Oʻahu, Molokaʻi , Lānaʻi, Maui, Kahoʻolawe and Hawaiʻi Island https:\/\/www.hvcb.org\/school-report\/eight-major-islands\/ . See   In addition, there are several atolls, and numerous smaller islets. It forms what we call an archipelago. Hawaiʻi became U.S. state on August 21, 1959 https:\/\/www.archives.gov\/legislative\/features\/hawaii , and is the only one not geographically connected to North America.    Map of the Hawaiian or Sandwich Islands, published in 1854 in Edward T. Perkins' Na Motu; or, Reef-Rovings in the South Seas.  Map of the Hawaiian ialnds in grey and white.   The formation of the Hawaiian Islands is occurring via Hot-Spot Volcanism. The fixed hot spot at the source of the Hawaiian Islands is located in the middle of the Pacific Plate, and the chain was formed as the plate drifted toward the northwest. Loihi, the youngest seamount of the chain is currently erupting from its summit at a depth of 1000 meters. In the following chart we have the age of some Hawaiian volcanoes along with their distance from Kilauea: https:\/\/pubs.usgs.gov\/pp\/1987\/1350\/pdf\/chapters\/pp1350_ch1.pdf      Volcano  Age in million years  Distance in km    Kilauea  0. 2  0    Mauna Kea  0.375  54    Kohala  0.43  100    Haleakala  0.75  182    Kahoolawe  1.03  185    West Maui  1.32  221    Lānaʻi  1.28  226    East Molokaʻi  1.76  256    West Molokaʻi  1.9  280    Koolau  2.6  339    Waianae  3.7  374    Kauaʻi  5.1  519    Niʻihau  5.89  565     This relation is an example of what we call a function which means that for each Island, given as input their age in million years we have an output in the form of their distance in km to the Hot Spot. It can be observed that from that the Island of Niihau is the oldest one from this list being 4.89 million years old and also the furthest one from the Hot Spot at 565 km. Koolau, Oʻahu Island, is estimated to be 2.6 million years old and at 339 km from the hot spot.    discusses a dramatic event that occurred on the Island of Kaho'olawe in Spring 2020.   In late February of 2020, Kahoʻolawe caught fire (see for an aerial view of the region that burned). Due to the danger of unexploded ordnances, firefighters were unable to actively fight the brush fire, which left it burning for roughly six days until a rainstorm stopped the spread. The fire burned much of the re-introduced vegetation on the island along with destroying the Kahoʻolawe Island Reserve Commission (KIRC) equipment and storage http:\/\/www.kahoolawe.hawaii.gov\/newsletters\/Spring_2020.pdf . is a transcript of a news report from Hawaiʻi Public Radio on the subject.      From a news report on Kahoʻolawe fire, Spring 2020 (source: Hawaiʻi Public Radio).   In that report units were critical to understand the time-line of the event, its magnitude, as well as the cost. Extracted from the text, we can see that the units quantifying the event are:  week ;  acres ;  years ;  dollars .    Aerial View of the region that caught fire. Taken by Prof. M. Chyba on January 29, 2023.     Unit Analysis   is the process of working with units to help solve problems.      Coming back to and . Answer the following questions about the units   What is the independent variable in this function? What are the units for the independent variable? Age is the independent variable and Million Years is the corresponding unit.  What is the dependent variable in this function? What are the units for the dependent variable? Distance is the dependent variable (the distance depend on the age) and kilometers (km) is the corresponding unit.  How much older and further away from the hot spot is Kauaʻi compare to Mauna Kea? Kauaʻi is  million years older than Mauna Kea  million years) and is  km further away from the volcanic hot spot (  km. ).   We finish this section with another example of units used as a descriptor of some specifics about the island of Kahoʻolawe.   Quantitative Information about Kahoʻolawe     Environmental data. The yearly temperature typically varies between F ( C) and F C) with the coolest in February and warmest in September. The largest rain precipitation is usually in January with an average of  inches (  mm ) and the driest month is June with an average of  inches (  mm ) https:\/\/geoportal.hawaii.gov\/    Geographical data. Its position on Earth is given by latitude:  N, longitude:  W. The island's dimension is  miles ( km ) long by  miles (  km ) across. It has a total land area of  square miles (  km ), and the highest elevation is the crater of Lua Makika at  feet (ft) (  meters ) >https:\/\/en.wikipedia.org\/wiki\/Kahoolawe    "
 },
 {
   "id": "ex-units-islands",
   "level": "2",
-  "url": "sec-units.html#ex-units-islands",
+  "url": "sec-units-intro.html#ex-units-islands",
   "type": "Exploration",
   "number": "1.1.1",
   "title": "",
@@ -39,7 +30,7 @@ var ptx_lunr_docs = [
 {
   "id": "ex-units-islands-kaho",
   "level": "2",
-  "url": "sec-units.html#ex-units-islands-kaho",
+  "url": "sec-units-intro.html#ex-units-islands-kaho",
   "type": "Exploration",
   "number": "1.1.2",
   "title": "",
@@ -48,7 +39,7 @@ var ptx_lunr_docs = [
 {
   "id": "def-unit-analysis",
   "level": "2",
-  "url": "sec-units.html#def-unit-analysis",
+  "url": "sec-units-intro.html#def-unit-analysis",
   "type": "Definition",
   "number": "1.1.5",
   "title": "Unit Analysis.",
@@ -57,7 +48,7 @@ var ptx_lunr_docs = [
 {
   "id": "sec-units-intro-7",
   "level": "2",
-  "url": "sec-units.html#sec-units-intro-7",
+  "url": "sec-units-intro.html#sec-units-intro-7",
   "type": "Example",
   "number": "1.1.6",
   "title": "<div class=\"para\" id=\"sec-units-intro-7-1-1\">Coming back to  Exploration 1.1.1 and Table 1.1.2. Answer the following questions about the units<\/div>.",
@@ -66,11 +57,20 @@ var ptx_lunr_docs = [
 {
   "id": "fact-unitK",
   "level": "2",
-  "url": "sec-units.html#fact-unitK",
+  "url": "sec-units-intro.html#fact-unitK",
   "type": "Exploration",
   "number": "1.1.3",
   "title": "Quantitative Information about Kahoʻolawe.",
   "body": " Quantitative Information about Kahoʻolawe     Environmental data. The yearly temperature typically varies between F ( C) and F C) with the coolest in February and warmest in September. The largest rain precipitation is usually in January with an average of  inches (  mm ) and the driest month is June with an average of  inches (  mm ) https:\/\/geoportal.hawaii.gov\/    Geographical data. Its position on Earth is given by latitude:  N, longitude:  W. The island's dimension is  miles ( km ) long by  miles (  km ) across. It has a total land area of  square miles (  km ), and the highest elevation is the crater of Lua Makika at  feet (ft) (  meters ) >https:\/\/en.wikipedia.org\/wiki\/Kahoolawe   "
+},
+{
+  "id": "sec-unit-conversion",
+  "level": "1",
+  "url": "sec-unit-conversion.html",
+  "type": "Subsection",
+  "number": "1.1.2",
+  "title": "Unit Conversion",
+  "body": " Unit Conversion  Text of section.  "
 },
 {
   "id": "backmatter-2",
